ae06d3c0d0830d7dc45defdd932c7c33dab2aaa4
nguyentn <nguyentn@fsoft.com.vn>
2016-08-03 20:27:47 +0700

    Update UI.

--- js/create-template.js
+++ js/create-template.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
     $("#btn-upload-file").click(function (evt) {
         /* For BE
         uploadSampleFile(input.files);
-		*/
+        /* */
         /* For FE */
         changeTab();
         /* */
@@ -54,12 +54,13 @@ $(document).ready(function () {
             url: "/Template/AddField",
             success: function (data) {
                 $('#box-fields').append(data);
-                drawStatus = 1;
+                var id = generateId(30);
+                $('#box-fields .box-field').last().attr('id', id);
             },
             error: function () {
             }
         });
-		*/
+        /* */
         /* For FE */
         $('#box-fields').append($('#box-field-temp').html());
         var id = generateId(30);
@@ -84,7 +85,7 @@ $(document).ready(function () {
         }
     });
 
-    $(document).on("click", ".field-name", function (e) {
+    $(document).on("click", ".txt-field-name", function (e) {
         canToggle = false;
     });
 
@@ -110,8 +111,12 @@ $(document).ready(function () {
     });
 
     $(document).on("click", ".btn-save-coordination", function (e) {
-        buildRectList($(this).closest('.box-field').attr('id'), false);
-        displayCoordinationBtns(this, "set");
+        var valid = validateCoordination(this, currentRect);
+        if (valid) {
+            buildRectList($(this).closest('.box-field').attr('id'), false);
+            // getPreviewForField();
+            displayCoordinationBtns(this, "set");
+		}
     });
 
     $(document).on("click", ".btn-cancel-coordination", function (e) {
@@ -133,6 +138,38 @@ $(document).ready(function () {
         removeItemOutOfRectList($(this).closest('.box-field').attr('id'));
         draw();
         $('#' + $(this).closest('.box-field').attr('id')).remove();
+    });
+	
+	$(document).on("click", "#btn-step2-next", function (e) {
+        collapseFields();
+        var valid = validateStep2();
+        if (valid) {
+            changeTab();
+        }
+    });
+
+
+
+    /* ---------------------------------------- Step 3 ---------------------------------------- */
+	$('#box-verification button').click(function(){
+		if($(this).hasClass('locked_active') || $(this).hasClass('unlocked_inactive')){
+			/* code to do when unlocking */
+			console.log('Switched on.');
+		}else{
+			/* code to do when locking */
+			console.log('Switched off.');
+		}
+		
+		/* reverse locking status */
+		$('#box-verification button').eq(0).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
+		$('#box-verification button').eq(1).toggleClass('locked_inactive locked_active btn-default btn-info');
+	});
+	
+	$(document).on("click", "#btn-step3-next", function (e) {
+        var valid = validateStep3();
+        if (valid) {
+            changeTab();
+        }
     });
 });
 
@@ -181,6 +218,14 @@ var changeTab = function () {
 
 /* ---------------------------------------- Step 1 ---------------------------------------- */
 var maxFileSize = (4 * 1024 * 1024);
+var sourceId;
+var acceptableFileTypes = 
+{
+    'application/pdf': true,
+    'image/png': true,
+    'image/jpg': true,
+    'image/jpeg': true,
+};
 
 var isAdvancedUpload = function () {
     var div = document.createElement('div');
@@ -210,7 +255,7 @@ var validateSampleFile = function (files) {
     } else {
         // Validate type of file
         var file = files[0];
-        if (file.type.indexOf('image/') > -1 || file.type.indexOf('application/pdf') > -1) {
+        if (acceptableFileTypes[file.type]) {
             if (file.size > maxFileSize) {
                 $('.file-selected').html(messageList.EM_003);
                 $('.file-selected').addClass('file-error');
@@ -239,7 +284,9 @@ var uploadSampleFile = function (files) {
             contentType: false,
             processData: false,
             data: data,
-            success: function (message) {
+            success: function (data) {
+                sourceId = data.sourceId;
+                $("#canvas").css("background-image", "url(" + data.base64 + ")");
                 changeTab();
             },
             error: function () {
@@ -306,6 +353,130 @@ var displayCoordinationBtnsOfAllField = function (id) {
             }
         }
     });
+}
+
+// Validate coordination before call OCR
+var validateCoordination = function (that, rect) {
+    if (rect.x == 0 && rect.y == 0 && rect.w == 0 && rect.h == 0) {
+		$(that).closest(".box-field").find(".txt-coordination-error").css("display", "inline-block");
+        $(that).closest(".box-field").find(".txt-coordination-error").html(messageList.EM_011);
+        return false;
+    } else {
+		$("#box-step-two .txt-coordination-error").css("display", "none");
+        return true;
+    }
+}
+
+// Get preview for field
+var getPreviewForField = function () {
+    $.ajax({
+        type: "POST",
+        url: "/Template/ExecuteScan",
+        data: {
+            fieldName: "nguyen",
+            x: 50,
+            y: 50,
+            w: 100,
+            h: 100,
+            sourceId: sourceId
+        },
+        headers: {
+            token: "foo"
+        },
+        success: function (data) {
+            sourceId = data.message;
+        },
+        error: function () {
+            alert("There was error uploading files!");
+        }
+    });
+};
+
+var validateStep2 = function () {
+	var valid = true;
+	var length = $("#box-fields .box-field").length;
+	if (length == 0) {
+		valid = false;
+		alert(messageList.EM_008);
+	}
+    $("#box-fields .box-field").each(function () {
+		var localValid = true;
+
+        var name = $(this).find(".txt-field-name")[0];
+        if (name.value.trim().length == 0) {
+			$(this).find(".txt-field-name").addClass("error");
+            $(this).find(".txt-field-name-error").css("display", "block");
+            $(this).find(".txt-field-name-error").html(messageList.EM_009);
+            localValid = false;
+        } else if (name.value.trim().length > 50) {
+			$(this).find(".txt-field-name").addClass("error");
+            $(this).find(".txt-field-name-error").css("display", "block");
+            $(this).find(".txt-field-name-error").html(messageList.EM_010);
+            localValid = false;
+        } else {
+			$(this).find(".txt-field-name").removeClass("error");
+            $(this).find(".txt-field-name-error").css("display", "none");
+        }
+
+        var isSet = checkCoordinationIsSet($(this).attr("id"));
+		if (!isSet) {
+			localValid = false;
+			$(this).find(".txt-coordination-error").css("display", "inline-block");
+			$(this).find(".txt-coordination-error").html(messageList.EM_012);
+		} else {
+			$(this).find(".txt-coordination-error").css("display", "none");
+		}
+		
+        if (!localValid) {
+            if ($(this).children(".box-field-body").css("display") == "none") {
+                $(this).find(".glyphicon").addClass("glyphicon-menu-up");
+				$(this).children(".box-field-body").toggle("fast");
+            }
+			valid = false;
+        }
+    });
+	return valid;
+}
+
+
+
+/* ---------------------------------------- Step 3 ---------------------------------------- */
+var validateStep3 = function () {
+    var valid = true;
+	
+    var templateName = $("#box-step-three #txt-template-name")[0];
+    if (templateName.value.trim().length == 0) {
+        $("#box-step-three #txt-template-name").addClass("error");
+        $("#box-step-three #txt-template-name-error").css("display", "block");
+        $("#box-step-three #txt-template-name-error").html(messageList.EM_013);
+        valid = false;
+    } else if (templateName.value.trim().length > 50) {
+        $("#box-step-three #txt-template-name").addClass("error");
+        $("#box-step-three #txt-template-name-error").css("display", "block");
+        $("#box-step-three #txt-template-name-error").html(messageList.EM_014);
+        valid = false;
+    } else {
+        $("#box-step-three #txt-template-name").removeClass("error");
+        $("#box-step-three #txt-template-name-error").css("display", "none");
+    }
+	
+	var templateDesc = $("#box-step-three #txt-template-description")[0];
+    if (templateDesc.value.trim().length == 0) {
+        $("#box-step-three #txt-template-description").addClass("error");
+        $("#box-step-three #txt-template-description-error").css("display", "block");
+        $("#box-step-three #txt-template-description-error").html(messageList.EM_015);
+        valid = false;
+    } else if (templateDesc.value.trim().length > 500) {
+        $("#box-step-three #txt-template-description").addClass("error");
+        $("#box-step-three #txt-template-description-error").css("display", "block");
+        $("#box-step-three #txt-template-description-error").html(messageList.EM_016);
+        valid = false;
+    } else {
+        $("#box-step-three #txt-template-description").removeClass("error");
+        $("#box-step-three #txt-template-description-error").css("display", "none");
+    }
+	
+    return valid;
 }
 
 
